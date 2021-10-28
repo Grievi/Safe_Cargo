34e88699eb6245e438016f5e5183023a873065dd
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-28 11:34:54 +0300

    add routing path app-routing.module.ts

--- practice-cargo/src/app/app-routing.module.ts
+++ practice-cargo/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'aboutus', component: AboutusComponent},
+  { path: 'sign-in', component: SignInComponent},
+  { path: 'sign-up', component: SignUpComponent},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
