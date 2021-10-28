2730ed482a7aa83d878fe1193d9e8e5f5290acbf
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-10-28 11:35:55 +0300

    add app.module.ts

--- practice-cargo/src/app/app.module.ts
+++ practice-cargo/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutusComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
